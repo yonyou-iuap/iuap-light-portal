da0f99dc8f360c6b9f7b5636f0d1c943739ecafb
LiuYueKai <liuyuekainihao@163.com>
2019-03-13 11:21:16 +0800

    修改打包路径

--- build.js
+++ build.js
@@ -5,16 +5,16 @@ const path = require('path')
 
 rm(path.resolve(__dirname, './build'), err => {
   fs.mkdirSync(path.resolve(__dirname, './build'))
-  fs.mkdirSync(path.resolve(__dirname, './build/light_frame_fe'))
-  fs.mkdirSync(path.resolve(__dirname, './build/light_frame_fe/pages'))
-  fs.mkdirSync(path.resolve(__dirname, './build/light_frame_fe/index'))
-  fs.mkdirSync(path.resolve(__dirname, './build/light_frame_fe/pages/login'))
-  cpr(path.resolve(__dirname, './ucf-publish/index/index.html'), path.resolve(__dirname, './build/light_frame_fe/index.html'), {}, err => {
-    cpr(path.resolve(__dirname, './ucf-publish/index/index.css'), path.resolve(__dirname, './build/light_frame_fe/index/index.css'), {}, err => {
-      cpr(path.resolve(__dirname, './ucf-publish/index/index.js'), path.resolve(__dirname, './build/light_frame_fe/index/index.js'), {}, err => {
-        cpr(path.resolve(__dirname, './ucf-publish/login'), path.resolve(__dirname, './build/light_frame_fe/pages/login'), {}, err => {
-          fs.rename('./build/light_frame_fe/pages/login/index.html', './build/light_frame_fe/pages/login/login.html')
-          let htmlPath = path.resolve(__dirname + '/build/light_frame_fe/index.html');
+  fs.mkdirSync(path.resolve(__dirname, './build/iuap-light-portal'))
+  fs.mkdirSync(path.resolve(__dirname, './build/iuap-light-portal/pages'))
+  fs.mkdirSync(path.resolve(__dirname, './build/iuap-light-portal/index'))
+  fs.mkdirSync(path.resolve(__dirname, './build/iuap-light-portal/pages/login'))
+  cpr(path.resolve(__dirname, './ucf-publish/index/index.html'), path.resolve(__dirname, './build/iuap-light-portal/index.html'), {}, err => {
+    cpr(path.resolve(__dirname, './ucf-publish/index/index.css'), path.resolve(__dirname, './build/iuap-light-portal/index/index.css'), {}, err => {
+      cpr(path.resolve(__dirname, './ucf-publish/index/index.js'), path.resolve(__dirname, './build/iuap-light-portal/index/index.js'), {}, err => {
+        cpr(path.resolve(__dirname, './ucf-publish/login'), path.resolve(__dirname, './build/iuap-light-portal/pages/login'), {}, err => {
+          fs.rename('./build/iuap-light-portal/pages/login/index.html', './build/iuap-light-portal/pages/login/login.html')
+          let htmlPath = path.resolve(__dirname + '/build/iuap-light-portal/index.html');
           let htmlStr = fs.readFileSync(htmlPath);
           htmlStr = htmlStr.toString().replace(/..\/index/g,'./index')
           fs.writeFileSync(htmlPath, htmlStr);
